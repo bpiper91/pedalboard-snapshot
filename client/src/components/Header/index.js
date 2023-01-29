05f6e73087de3fafafc3bde69a118136d4ebb8b4
bpiper91 <bpiper91@gmail.com>
2023-01-28 19:01:21 -0500

    added placeholder header and footer components, removed/updated default content in App.js, index.js, and index.html

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <header>
+            <div>
+                <h1>Pedalboard Snapshot</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
